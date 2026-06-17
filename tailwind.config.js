37d33e032282bbb081518ddd9ed263436b769284
nikolozchaganava <nikolozchaganava333@gmail.com>
2026-06-17 18:38:15 +0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        georgian: ['"Noto Serif Georgian"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
